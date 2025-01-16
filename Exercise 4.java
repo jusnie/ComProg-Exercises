3b9a687429ab1f2d27942fdc8a8dc1ef91e6e616
Juanie Cuenca <123187548+jusnie@users.noreply.github.com>
2025-01-16 23:29:00 +0800

    Create Exercise 4.java

---
+++ Exercise 4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;    
+    class Main {
+    public static void main(String[] args) {
+        System.out.println ("(JavaTerminology (this will transferon our ide ))  ");
+        System.out.println();
+        System.out.println("Class: A blueprint for defining attributes and behaviors.");
+        System.out.println();
+        System.out.println("Object: An instance of a class.");
+        System.out.println();
+        System.out.println("Inheritance: Allows one class to use the properties of another.");
+        System.out.println();
+        System.out.println("Polymorphism: A method behaving differently based on context.");
+        System.out.println();
+        System.out.println("Encapsulation: Restricts access to object components.");
+// we struglled in this bcus we need to find the specific findings we need to find thankfully it's System.out.print only
+    }
+    }
